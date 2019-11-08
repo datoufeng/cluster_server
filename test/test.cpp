130ed1b4e716b14ee3f7edf423fc5b5deda44ced
feng <feng@qq.com>
2019-11-08 16:20:42 +0800

    test.cpp test.h  test.txt

--- test/test.cpp
+++ test/test.cpp
@@ -1,23 +1,5 @@
-#include "../data_translate.h"
-struct t_data{
-	string name;
-	int id;
-	string address;
-	string education;
-};
-
-class t_channel:public channel{
-	
-};
-
-class t_protocol:public protocol{
-
-};
-
-class t_dealer:public dealer{
-
-};
-
+#include "test.h"
 int main(){
 
 }
+
